1ebe272256130cb563909f7ec8f5c20ddeeff679
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-13 19:35:27 +0530

    Created folders

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Login.css'
+import assets from '../../assets/assets'
+
+export const Login = () => {
+  return (
+    <div className='login' >
+        <img src={assets.logo_big} alt="" className='logo' />
+    </div>
+  )
+}
