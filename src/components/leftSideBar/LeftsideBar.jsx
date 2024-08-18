889cf1d4162579b8bf881c0da0cefedc1a1720e6
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-18 21:35:19 +0530

    working on Chatbox

--- src/components/leftSideBar/LeftsideBar.jsx
+++ src/components/leftSideBar/LeftsideBar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import './LeftSideBar.css'
+import React from "react";
+import "./LeftSideBar.css";
+import assets from "../../assets/assets";
 const LeftsideBar = () => {
   return (
-    <div>LeftsideBar</div>
-  )
-}
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} alt="" className="logo" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+            <img src={assets.search_icon} alt="" />
+            <input type="text" placeholder="Search Here" />
+        </div>
+      </div>
+      <div className="ls-list">
+        <div className="friends">
+            <img src={assets.profile_img} alt="" />
+            <div>
+                <p>Madan Mohan</p>
+                <span>Hello How are you</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LeftsideBar
+export default LeftsideBar;
