4619cc2d8932e99662c98353fc7eadb528dccddd
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-19 00:51:03 +0530

    working on Chatbox

--- src/components/chatBox/chatBox.jsx
+++ src/components/chatBox/chatBox.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './chatBox.css'
+import assets from '../../assets/assets'
 
 const ChatBox = () => {
   return (
-    <div>
-chatbox
+    <div className='chat-box'>
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>John Doe <img className='dot' src={assets.green_dot} alt="" /></p>
+        <img src={assets.help_icon} className='help' alt="" />
+      </div>
+
     </div>
   )
 }
