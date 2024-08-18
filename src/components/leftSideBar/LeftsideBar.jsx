8af2326ed341e207a50db93ea3e4657eaf20f164
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-18 21:17:50 +0530

    Started working on Chatbox

---
+++ src/components/leftSideBar/LeftsideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './LeftSideBar.css'
+const LeftsideBar = () => {
+  return (
+    <div>LeftsideBar</div>
+  )
+}
+
+export default LeftsideBar
--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './chat.css'
+import LeftsideBar from '../../components/leftSideBar/LeftsideBar'
+import ChatBox from '../../components/chatBox/chatBox'
+import RightSideBar from '../../components/rightSideBar/RightSideBar'
 
 const Chat = () => {
   return (
-    <div>chat</div>
+    <div className='chat'>
+      <div className='chat-container'>
+        <LeftsideBar/>
+        <ChatBox/>
+        <RightSideBar/>
+      </div>
+    </div>
   )
 }
 
