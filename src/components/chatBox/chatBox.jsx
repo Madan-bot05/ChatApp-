23bd884e5834db4e9d7e82dd6688a2c6dd432dc8
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-20 00:28:42 +0530

    working on chatbox

--- src/components/chatBox/chatBox.jsx
+++ src/components/chatBox/chatBox.jsx
@@ -11,13 +11,32 @@ const ChatBox = () => {
         <img src={assets.help_icon} className='help' alt="" />
       </div>
 
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <div>
+          <img src={assets.profile_img} alt="" />
+          <p>2:30pm</p>
+        </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <div>
+          <img src={assets.profile_img} alt="" />
+          <p>2:30pm</p>
+        </div>
+        </div>
+      </div>
+
+
       <div className="chat-input">
         <input type="text" placeholder='send a message' />
         <input type="file" id='image' accept='image/png, image/jepg' hidden />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" />
-          <img src={assets.send_button} alt="" />
         </label>
+        <img src={assets.send_button} alt="" />
       </div>
     </div>
   )
