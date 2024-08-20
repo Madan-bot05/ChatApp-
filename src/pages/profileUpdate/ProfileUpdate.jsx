a30e8124c891cc5d4b484958d3a8cdc3342e5384
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-21 01:23:26 +0530

    working on profile update page

--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from "../../assets/assets";
+
 const ProfileUpdate = () => {
   return (
-    <div>ProfileUpdate</div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form action="">
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" name="" id="avatar" accept='.png,.jpg,.jpeg' hidden/>
+            <img src={assets.avatar_icon} alt="" />
+            upload profile image
+          </label>
+          <input type="text" placeholder='Your Name' required />
+          <textarea placeholder='Write Profile Bio'></textarea>
+          <button type='submit'></button>
+        </form>
+        <img src={assets.logo_icon} alt="" />
+      </div>
+
+    </div>
   )
 }
 
