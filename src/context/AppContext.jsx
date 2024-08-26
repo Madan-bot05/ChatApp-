b08cee5c9c3c7da462bc83b8ac631c239d83c738
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-27 01:10:11 +0530

    working on bio update page

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,11 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { createContext, useState } from "react";
-import { db } from "../config/fireBase";
+import { auth, db } from "../config/fireBase";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext=createContext()
 const AppContextProvider=(props)=>{
+    const navigate=useNavigate();
     const [userData,setUserData]=useState(null);
     const [chatData,setChatData]=useState(null);
 
@@ -12,7 +14,22 @@ const AppContextProvider=(props)=>{
             const userRef=doc(db,'users',uid)
             const userSnap=await getDoc(userRef)
             const userData=userSnap.data()
-            console.log(userData)
+            setUserData(userData)
+            if (userData.avatar && userData.name) {
+                navigate('/chat')
+            } else {
+                navigate('profile')
+            }
+            await updateDoc(userRef,{
+                lastSeen:Date.now()
+            })
+            setInterval(async()=>{
+                if(auth.chatUser){
+                    await updateDoc(userRef,{
+                        lastSeen:Date.now()
+                    })
+                }
+            },60000)
         } catch (error) {
             
         }
--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,25 +1,73 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProfileUpdate.css'
 import assets from "../../assets/assets";
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth ,db} from '../../config/fireBase';
+import { doc, getDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const ProfileUpdate = () => {
 
+  const navigate=useNavigate();
+
+
   const [image ,setImage]=useState(false)
+  const [name,setName]=useState("")
+  const [bio,setBio]=useState("")
+  const [uid,setUid]=useState("")
+  const [prevImage,setPrevImage]=useState("")
+
+  const profileUpdate=async(event)=>{
+    event.preventDefault();
+    try {
+      if(!prevImage && !image){ toast.error("Please select an image") }
+      const docRef=doc(db,"users",uid)
+
+      if(image){}
+      else{}
+
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,async(user)=>{
+      if(user){
+        setUid(user.uid)
+        const docRef=doc(db,"users",user.uid)
+        const docSnap=getDoc(docRef)
+        if((await docSnap).data().name){
+          setName((await docSnap).data().name)
+        }
+        if((await docSnap).data().bio){
+          setName((await docSnap).data().bio)
+      }
+        if((await docSnap).data().avatar){
+          setPrevImage((await docSnap).data().avatar)
+      }
+    }
+    else{
+      navigate('/')
+    }
+  })
+  })
 
 
 
   return (
     <div className='profile'>
       <div className="profile-container">
-        <form action="">
+        <form action="" onSubmit={profileUpdate}>
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
             <input onChange={(e)=>setImage(e.target.files[0])} type="file" name="" id="avatar" accept='.png,.jpg,.jpeg' hidden/>
             <img src={image?URL.createObjectURL(image):assets.avatar_icon} alt="" />
             upload profile image
           </label>
-          <input type="text" placeholder='Your Name' required />
-          <textarea placeholder='Write Profile Bio'></textarea>
+          <input onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Your Name' required />
+          <textarea onChange={(e)=>setBio(e.target.value)} value={bio} placeholder='Write Profile Bio'></textarea>
           <button type='submit'>save</button>
         </form>
         <img className='profile-pic' src={image?URL.createObjectURL(image):assets.logo_icon} alt="" />
